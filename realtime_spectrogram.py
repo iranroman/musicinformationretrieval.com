05569141fcffa2af22cf985552d5202d895a076f
Steve Tjoa <stjoa@ayzenberg.com>
2018-07-06 13:10:59 -0700

    real time spectrogram

--- realtime_spectrogram.py
+++ realtime_spectrogram.py
@@ -1,7 +1,7 @@
 """
 musicinformationretrieval.com/realtime_spectrogram.py
 
-PyAudio example: display a live spectrogram in the terminal.
+PyAudio example: display a live log-spectrogram in the terminal.
 
 For more examples using PyAudio:
     https://github.com/mwickert/scikit-dsp-comm/blob/master/sk_dsp_comm/pyaudio_helper.py
@@ -11,30 +11,46 @@ import numpy
 import pyaudio
 import time
 
+# Define global variables.
 CHANNELS = 1
 RATE = 44100
 FRAMES_PER_BUFFER = 1000
 N_FFT = 4096
 SCREEN_WIDTH = 178
+ENERGY_THRESHOLD = 0.4
 
-BIN_LO = N_FFT*librosa.note_to_hz('C2')/RATE
-BIN_HI = N_FFT*librosa.note_to_hz('C7')/RATE
-FREQ_BINS = numpy.geomspace(BIN_LO, BIN_HI, SCREEN_WIDTH).astype('int')
+# Choose the frequency range of your log-spectrogram.
+F_LO = librosa.note_to_hz('C2')
+F_HI = librosa.note_to_hz('C9')
+M = librosa.filters.mel(RATE, N_FFT, SCREEN_WIDTH, fmin=F_LO, fmax=F_HI)
 
 p = pyaudio.PyAudio()
 
 def generate_string_from_audio(audio_data):
+    """
+    This function takes one audio buffer as a numpy array and returns a
+    string to be printed to the terminal.
+    """
+    # Compute real FFT.
     x_fft = numpy.fft.rfft(audio_data, n=N_FFT)
-    X = numpy.log10(1 + 0.1*abs(x_fft))
 
+    # Compute mel spectrum.
+    melspectrum = M.dot(abs(x_fft))
+
+    # Initialize output characters to display.
     char_list = [' ']*SCREEN_WIDTH
 
-    for i in range(len(FREQ_BINS)):
-        b = FREQ_BINS[i]
-        if X[b] > 0.3:
+    for i in range(SCREEN_WIDTH):
+
+        # If there is energy in this frequency bin, display an asterisk.
+        if melspectrum[i] > ENERGY_THRESHOLD:
             char_list[i] = '*'
+
+        # Draw frequency axis guidelines.
         elif i % 30 == 29:
             char_list[i] = '|'
+
+    # Return string.
     return ''.join(char_list)
 
 def callback(in_data, frame_count, time_info, status):
@@ -45,8 +61,8 @@ def callback(in_data, frame_count, time_info, status):
 stream = p.open(format=pyaudio.paFloat32,
                 channels=CHANNELS,
                 rate=RATE,
-                input=True,
-                output=True,
+                input=True,   # Do record input.
+                output=False, # Do not play back output.
                 frames_per_buffer=FRAMES_PER_BUFFER,
                 stream_callback=callback)
 
