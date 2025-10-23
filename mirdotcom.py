2cf5f052788fde4ed613e8b4d13021b3b58fdd2d
huwcheston <hwc31@cam.ac.uk>
2025-10-23 15:07:26 +0100

    mirdotcom.py changes

--- mirdotcom.py
+++ mirdotcom.py
@@ -6,10 +6,40 @@ from time import sleep
 import matplotlib.pyplot as plt
 import requests
 
-API_URL = "https://api.github.com/repos/iranroman/musicinformationretrieval.com/git/trees/gh-pages?recursive=1"
 BASE_URL = "https://musicinformationretrieval.com"
 DOWNLOAD_ATTEMPTS = 10
 WAIT_SECONDS = 2
+
+ASSETS = [
+    'audio/125_bounce.wav',
+    'audio/58bpm.wav',
+    'audio/brahms_hungarian_dance_5.mp3',
+    'audio/busta_rhymes_hits_for_days.mp3',
+    'audio/c_strum.wav',
+    'audio/clarinet_c6.wav',
+    'audio/classic_rock_beat.mp3',
+    'audio/classic_rock_beat.wav',
+    'audio/conga_groove.wav',
+    'audio/cowbell.wav',
+    'audio/funk_groove.mp3',
+    'audio/jangle_pop.mp3',
+    'audio/latin_groove.mp3',
+    'audio/oboe_c6.wav',
+    'audio/prelude_cmaj.wav',
+    'audio/simple_loop.wav',
+    'audio/simple_piano.wav',
+    'audio/sir_duke_piano_fast.mp3',
+    'audio/sir_duke_piano_slow.mp3',
+    'audio/sir_duke_trumpet_fast.mp3',
+    'audio/sir_duke_trumpet_slow.mp3',
+    'audio/thx_original.mp3',
+    'audio/tone_440.wav',
+    *[f"audio/drum_samples/test/kick_{str(i).zfill(2)}.mp3" for i in range(62)],
+    *[f"audio/drum_samples/test/snare_{str(i).zfill(2)}.mp3" for i in range(32)],
+    *[f"audio/drum_samples/train/kick_{str(i).zfill(2)}.mp3" for i in range(1, 11)],
+    *[f"audio/drum_samples/train/snare_{str(i).zfill(2)}.mp3" for i in range(1, 11)],
+    'img/classic_rock_beat_drumtab.png'
+]
 
 
 def init():
@@ -34,21 +64,6 @@ def set_plt_style():
     plt.rcParams['axes.ymargin'] = 0
     plt.rcParams['image.cmap'] = 'gray'
     plt.rcParams['image.interpolation'] = "none"
-
-
-def download_tree_data(attempts: int = DOWNLOAD_ATTEMPTS):
-    for _ in range(attempts):
-        # Get the JSON tree data from GitHub API
-        try:
-            response = requests.get(API_URL)
-            response.raise_for_status()
-            data = response.json()
-        except:
-            sleep(WAIT_SECONDS)
-            continue
-        else:
-            return data
-    raise ValueError("No data returned, try again later")
 
 
 def download_file(path, attempts: int = DOWNLOAD_ATTEMPTS):
@@ -79,18 +94,13 @@ def download_file(path, attempts: int = DOWNLOAD_ATTEMPTS):
 
 
 def download_audio():
-    data = download_tree_data()
     # Filter paths that are blobs (files) and inside 'audio/' folder
-    paths = [item['path'] for item in data['tree'] if item['type'] == 'blob' and item['path'].startswith('assets/audio/')]
-
+    paths = [os.path.join("assets", item) for item in ASSETS if item.startswith("audio/")]
     for path in paths:
         download_file(path)
 
 def download_images():
-    data = download_tree_data()
-
-    # Filter paths that are blobs (files) and inside 'audio/' folder
-    paths = [item['path'] for item in data['tree'] if item['type'] == 'blob' and item['path'].startswith('assets/img/')]
-
+    # Filter paths that are blobs (files) and inside 'image/' folder
+    paths = [os.path.join("assets", item) for item in ASSETS if item.startswith("img/")]
     for path in paths:
         download_file(path)
