03b8fe55bc9a3d31292ed7cc15fb03aa022f684f
huwcheston <hwc31@cam.ac.uk>
2025-10-31 15:36:48 +0000

    run black on files

--- mirdotcom/content/11_fun/realtime_spectrogram.py
+++ mirdotcom/content/11_fun/realtime_spectrogram.py
@@ -6,6 +6,7 @@ PyAudio example: display a live log-spectrogram in the terminal.
 For more examples using PyAudio:
     https://github.com/mwickert/scikit-dsp-comm/blob/master/sk_dsp_comm/pyaudio_helper.py
 """
+
 import librosa
 import numpy
 import pyaudio
@@ -20,11 +21,12 @@ SCREEN_WIDTH = 178
 ENERGY_THRESHOLD = 0.4
 
 # Choose the frequency range of your log-spectrogram.
-F_LO = librosa.note_to_hz('C2')
-F_HI = librosa.note_to_hz('C9')
+F_LO = librosa.note_to_hz("C2")
+F_HI = librosa.note_to_hz("C9")
 M = librosa.filters.mel(RATE, N_FFT, SCREEN_WIDTH, fmin=F_LO, fmax=F_HI)
 
 p = pyaudio.PyAudio()
+
 
 def generate_string_from_audio(audio_data):
     """
@@ -38,33 +40,37 @@ def generate_string_from_audio(audio_data):
     melspectrum = M.dot(abs(x_fft))
 
     # Initialize output characters to display.
-    char_list = [' ']*SCREEN_WIDTH
+    char_list = [" "] * SCREEN_WIDTH
 
     for i in range(SCREEN_WIDTH):
 
         # If there is energy in this frequency bin, display an asterisk.
         if melspectrum[i] > ENERGY_THRESHOLD:
-            char_list[i] = '*'
+            char_list[i] = "*"
 
         # Draw frequency axis guidelines.
         elif i % 30 == 29:
-            char_list[i] = '|'
+            char_list[i] = "|"
 
     # Return string.
-    return ''.join(char_list)
+    return "".join(char_list)
+
 
 def callback(in_data, frame_count, time_info, status):
     audio_data = numpy.fromstring(in_data, dtype=numpy.float32)
-    print( generate_string_from_audio(audio_data) )
+    print(generate_string_from_audio(audio_data))
     return (in_data, pyaudio.paContinue)
 
-stream = p.open(format=pyaudio.paFloat32,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,   # Do record input.
-                output=False, # Do not play back output.
-                frames_per_buffer=FRAMES_PER_BUFFER,
-                stream_callback=callback)
+
+stream = p.open(
+    format=pyaudio.paFloat32,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,  # Do record input.
+    output=False,  # Do not play back output.
+    frames_per_buffer=FRAMES_PER_BUFFER,
+    stream_callback=callback,
+)
 
 stream.start_stream()
 
--- mirdotcom/mirdotcom.py
+++ mirdotcom/mirdotcom.py
@@ -18,16 +18,16 @@ def init():
 
 
 def set_plt_style():
-    plt.style.use('seaborn-v0_8-muted')
-    plt.rcParams['axes.grid'] = True
-    plt.rcParams['axes.spines.left'] = False
-    plt.rcParams['axes.spines.right'] = False
-    plt.rcParams['axes.spines.bottom'] = False
-    plt.rcParams['axes.spines.top'] = False
-    plt.rcParams['axes.xmargin'] = 0
-    plt.rcParams['axes.ymargin'] = 0
-    plt.rcParams['image.cmap'] = 'gray'
-    plt.rcParams['image.interpolation'] = "none"
+    plt.style.use("seaborn-v0_8-muted")
+    plt.rcParams["axes.grid"] = True
+    plt.rcParams["axes.spines.left"] = False
+    plt.rcParams["axes.spines.right"] = False
+    plt.rcParams["axes.spines.bottom"] = False
+    plt.rcParams["axes.spines.top"] = False
+    plt.rcParams["axes.xmargin"] = 0
+    plt.rcParams["axes.ymargin"] = 0
+    plt.rcParams["image.cmap"] = "gray"
+    plt.rcParams["image.interpolation"] = "none"
 
 
 def set_working_directory():
